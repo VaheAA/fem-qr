3952bf2a6b39413c19639399b79af737e124fd96
VaheAA <vahe.abov@gmail.com>
2022-01-21 19:59:09 +0400

    html generatation from JS

---
+++ app.js
@@ -0,0 +1,26 @@
+const cardComponent = document.querySelector('.card-wrapper');
+
+window.addEventListener('DOMContentLoaded', () => {
+  cardComponent.innerHTML = generateCard();
+});
+
+function generateCard() {
+  return `        <div class="card">
+          <div class="card__head">
+            <img
+              class="card__qr"
+              src="assets/image-qr-code.png"
+              alt="QR code image"
+            />
+          </div>
+          <div class="card__body">
+            <h1 class="card__title">
+              Improve your front-end skills by building projects
+            </h1>
+            <p class="card__text">
+              Scan the QR code to visit Frontend Mentor and take your coding
+              skills to the next level
+            </p>
+          </div>
+        </div>`;
+}
